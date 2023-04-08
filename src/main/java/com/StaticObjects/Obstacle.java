a3bde50c535864a1a5770f4cbfb1c850712b66e5
Jakub <jakubplewa00@gmail.com>
2023-04-09 01:01:23 +0200

    v0.1.0

--- src/main/java/com/StaticObjects/Obstacle.java
+++ src/main/java/com/StaticObjects/Obstacle.java
@@ -1,4 +1,19 @@
 package com.StaticObjects;
 
+import com.GUI.GamePanel;
+
+import java.awt.*;
+import java.util.Random;
+
 public class Obstacle {
+
+    private int x;
+    private int y;
+
+    public void setPosition() {
+        Random random = new Random();
+        x = random.nextInt(GamePanel.getBoardSize());
+        y = random.nextInt(GamePanel.getBoardSize());
+        GamePanel.setObject(x, y, Color.DARK_GRAY);
+    }
 }
