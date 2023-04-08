7a352cb1164ad2fd9643a20bc87a204d4e6db04f
Jakub <jakubplewa00@gmail.com>
2023-04-09 00:53:22 +0200

    v0.1.0

--- src/main/java/com/DynamicObjects/enemies/Zombie.java
+++ src/main/java/com/DynamicObjects/enemies/Zombie.java
@@ -1,12 +1,16 @@
 package com.DynamicObjects.enemies;
 
 import com.DynamicObjects.Entity;
+import com.GUI.GamePanel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Zombie extends Entity implements ActionListener {
+
+    private final Color ZOMBIE = Color.green;
 
     public Zombie(int dmg, int hp) {
         setDmg(dmg);
@@ -19,12 +23,65 @@ public class Zombie extends Entity implements ActionListener {
 
     @Override
     public void setPosition() {
-
+        int newX, newY;
+        do {
+            do {
+                newX = random.nextInt(GamePanel.getBoardSize());
+                newY = random.nextInt(GamePanel.getBoardSize());
+            } while (GamePanel.checkOutOfBorder(newX, newY));
+        } while (GamePanel.checkPosition(newX, newY));
+        setX(newX);
+        setY(newY);
+        GamePanel.setObject(getX(), getY(), ZOMBIE);
     }
 
     @Override
     public void move() {
-
+        int newX = 0, newY = 0;
+        int choice;
+        do {
+            do {
+                choice = random.nextInt(8) + 1;
+                switch (choice) {
+                    case 1 -> {
+                        newX = getX() + 1;
+                        newY = getY() + 1;
+                    }
+                    case 2 -> {
+                        newX = getX() - 1;
+                        newY = getY() - 1;
+                    }
+                    case 3 -> {
+                        newX = getX();
+                        newY = getY() + 1;
+                    }
+                    case 4 -> {
+                        newX = getX() + 1;
+                        newY = getY();
+                    }
+                    case 5 -> {
+                        newX = getX() - 1;
+                        newY = getY();
+                    }
+                    case 6 -> {
+                        newX = getX();
+                        newY = getY() - 1;
+                    }
+                    case 7 -> {
+                        newX = getX() + 1;
+                        newY = getY() - 1;
+                    }
+                    case 8 -> {
+                        newX = getX() - 1;
+                        newY = getY() + 1;
+                    }
+                }
+            } while (GamePanel.checkOutOfBorder(newX, newY));
+        } while (GamePanel.checkPosition(newX, newY));
+        GamePanel.setObject(getX(), getY(), GamePanel.getBoardColor());
+        setX(newX);
+        setY(newY);
+        GamePanel.setObject(getX(), getY(), ZOMBIE);
     }
 
     @Override
--- src/main/java/com/Functional/GameController.java
+++ src/main/java/com/Functional/GameController.java
@@ -1,11 +1,13 @@
 package com.Functional;
 
 import com.DynamicObjects.*;
+import com.DynamicObjects.enemies.*;
 import com.GUI.GamePanel;
 
 public class GameController {
 
     private final Human[] human = new Human[3];
+    private final Entity[] entities = new Entity[6];
     private static GameController instance = null;
 
     public static GameController getInstance() {
@@ -17,8 +19,12 @@ public class GameController {
 
     private GameController() {
         for (int i = 0; i < 3; i++) {
-            human[i] = new Human(0,100);
+            human[i] = new Human(0, 100);
             human[i].setPosition();
+        }
+        for (int i = 0; i < 6; i++) {
+            entities[i] = new Zombie(0, 0);
+            entities[i].setPosition();
         }
     }
 
@@ -28,17 +34,26 @@ public class GameController {
         for (int i = 0; i < 3; i++) {
             human[i].setPosition();
         }
+        for (int i = 0; i < 6; i++) {
+            entities[i].setPosition();
+        }
     }
 
     public void startMove() {
         for (int i = 0; i < 3; i++) {
             human[i].start();
         }
+        for (int i = 0; i < 6; i++) {
+            entities[i].start();
+        }
     }
 
     public void stopMove() {
         for (int i = 0; i < 3; i++) {
             human[i].stop();
+        }
+        for (int i = 0; i < 6; i++) {
+            entities[i].stop();
         }
     }
 }
